b6fc2cb323447a265af03660d0c4330eac9d62f5
Dana Prakoso Eka Saputra <dana.best.programmer@gmail.com>
2024-01-28 06:19:12 +0000

    Last commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'dart:html';
+import 'dart:ui' as ui;
+
+void main() {
+  ui.platformViewRegistry.registerViewFactory(
+      'hello-world-html',
+          (int viewId) => IFrameElement()
+        ..width = '640'
+        ..height = '360'
+        ..src = 'assets/assets/splash.html'
+        ..style.border = 'none');
+
+  runApp(Directionality(
+    textDirection: TextDirection.ltr,
+    child: SizedBox(
+      width: 640,
+      height: 360,
+      child: HtmlElementView(viewType: 'hello-world-html'),
+    ),
+  ));
+}

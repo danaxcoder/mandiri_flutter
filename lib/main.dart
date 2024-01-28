3f75beaad6a026cc2accf4bd39bd6aee80a3c79a
Dana Prakoso Eka Saputra <dana.best.programmer@gmail.com>
2024-01-28 14:45:26 +0800

    Last commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
           (int viewId) => IFrameElement()
         ..width = '640'
         ..height = '360'
-        ..src = 'assets/assets/splash.html'
+        ..src = 'assets/assets/home.html'
         ..style.border = 'none');
 
   runApp(Directionality(
